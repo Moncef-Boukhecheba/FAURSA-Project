f1b5ef853c3ca67e4660f974dfb9076af59fae45
Moncef Boukhecheba <moncefbkb@gmail.com>
2022-07-05 12:04:02 +0100

    add hector-mapping :sparkles: + configure lama_ros (WIP) :bug:

--- first_setup.sh
+++ first_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-sudo apt install ros-noetic-map-server ros-noetic-amcl ros-noetic-gmapping
+sudo apt install ros-noetic-map-server ros-noetic-amcl ros-noetic-gmapping ros-noetic-hector-mapping
 
 # Build workspaces and source setup files 
 source make_projects.sh
