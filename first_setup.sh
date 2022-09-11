b73df3887e5b43961d68ad2a20bbe8f429fa2bfe
Moncef Boukhecheba <moncefbkb@gmail.com>
2022-09-11 17:23:02 +0100

    add dependency for gazebo2Dmap

--- first_setup.sh
+++ first_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install dependencies
-sudo apt install ros-noetic-map-server ros-noetic-amcl ros-noetic-gmapping ros-noetic-hector-mapping
+sudo apt install ros-noetic-map-server ros-noetic-amcl ros-noetic-gmapping ros-noetic-hector-mapping ros-noetic-navigation
 
 # Build workspaces and source setup files 
 source make_projects.sh
